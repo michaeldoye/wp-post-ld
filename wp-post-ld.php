2013a323551a5f58f597e76dc7bc2d46b6073483
michael <michaeldoye@gmail.com>
2018-07-30 15:41:58 +0200

    first commit

--- wp-post-ld.php
+++ wp-post-ld.php
@@ -1,90 +1,92 @@
 <?php
 /*
-Plugin Name: WC Product LD JSON
+Plugin Name: WP Post LD JSON
 Plugin URI: https://webseo.co.za
-Description: Adds relevant LD JSON to prodcut pages
+Description: Adds relevant LD JSON to post pages
 Author: Web SEO Online (PTY) LTD
 Author URI: https://webseo.co.za
 Version: 0.0.1
 
-	Copyright: © 2016 Web SEO Online (PTY) LTD (email : michael@webseo.co.za)
+	Copyright: © 2016 Web SEO Online (PTY) LTD (email : support@webseo.co.za)
 	License: GNU General Public License v3.0
 	License URI: http://www.gnu.org/licenses/gpl-3.0.html
 */
 
 
+/**
+* Make sure class doesn't already exist
+*/
 
-if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) { 
-
+if ( ! class_exists( 'WP_Post_LD' ) ) {
+	
 	/**
-	* Make sure class doesn't already exist
-	*/
+	* Localisation
+	**/
+	load_plugin_textdomain( 'WP_Post_LD', false, dirname( plugin_basename( __FILE__ ) ) . '/' );
 
-	if ( ! class_exists( 'WC_Product_ld' ) ) {
-		
+	class WP_Post_LD {
+
 		/**
-		* Localisation
-		**/
-		load_plugin_textdomain( 'WC_Product_ld', false, dirname( plugin_basename( __FILE__ ) ) . '/' );
-
-		class WC_Product_ld {
-
-			/**
-			* constructor
-			*/
-			public function __construct() {
-				add_filter( 'wp_head', array( $this, 'add_ld_script') );	            			
-			}
-
-			/**
-			* add_ld_script
-			* Checks post type and injects JSON LD data.
-			**/
-			public function add_ld_script() {
-				global $woocommerce, $post;
-
-				$product = wc_get_product( $post->ID );
-				$terms = get_the_terms( $post->ID, 'product_cat' );
-				$image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' );
-
-				if ( is_product() ) { ?>
-					<script type="application/ld+json">
-					{
-						"@context": "http://schema.org/",
-						"@type": "Product",
-						"name": "<?php echo $product->get_title() ?>",
-						"image": "<?php echo $image[0] ?>",
-						"description": "<?php echo get_post( $post->ID )->post_content ?>",
-						"mpn": "<?php echo $product->get_sku() ?>",
-						"brand": {
-							"@type": "Thing",
-							"name": "<?php echo $terms[0]->name ?>"
-						},
-						"aggregateRating": {
-							"@type": "AggregateRating",
-							"ratingValue": "<?php echo $product->get_average_rating() ?>",
-							"reviewCount": "<?php echo $product->get_review_count() ?>"
-						},
-						"offers": {
-							"@type": "Offer",
-							"priceCurrency": "ZAR",
-							"price": "<?php echo $product->get_price() ?>",
-							"itemCondition": "http://schema.org/UsedCondition",
-							"availability": "http://schema.org/InStock",
-							"seller": {
-							"@type": "Organization",
-							"name": "Executive Objects"
-							}
-						}
-					}
-					</script>
-				<?php } 
-			}
-
+		* constructor
+		*/
+		public function __construct() {
+			add_filter( 'wp_head', array( $this, 'add_ld_script') );	            			
 		}
-		
-		// finally instantiate our plugin class and add it to the set of globals
-		$GLOBALS['WC_Product_ld'] = new WC_Product_ld();
-	}
 
+		/**
+		* add_ld_script
+		* Checks post type and injects JSON LD data.
+		**/
+		public function add_ld_script() {
+			global $post;
+
+			$image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' );
+			$tags = array();
+			foreach ( get_the_tags( $post->ID ) as $key => $value ) {
+				$tags[$key] = $value->name;
+			}
+			$categories = get_the_category();
+
+			if ( is_single() && $post->post_type === "post" ) { ?>
+				<script type="application/ld+json">
+				{
+					"@context": "http:\/\/schema.org",
+					"@type": "BlogPosting",
+					"mainEntityOfPage": {
+						"@type": "WebPage",
+						"@id": "<?php echo $post->ID ?>"
+					},
+					"headline": "<?php echo $post->post_title ?>",
+					"image": "<?php echo $image[0] ?>",
+					"award": "Best article ever written",
+					"editor": "<?php echo the_author_meta( 'user_nicename' , $post->post_author ) ?>",
+					"genre": "<?php echo esc_html( $categories[0]->name ) ?>",
+					"keywords": "<?php echo implode( ",", $tags ) ?>",
+					"publisher": {
+						"@type": "Organization",
+						"name": "<?php echo the_author_meta( 'user_nicename' , $post->post_author ) ?>",
+						"logo": {
+							"@type": "ImageObject",
+							"url": ""
+						}
+					},
+					"url": "<?php echo get_permalink( $post->ID ) ?>",
+					"datePublished": "<?php echo $post->post_date ?>",
+					"dateCreated": "<?php echo $post->post_date ?>",
+					"dateModified": "<?php echo $post->post_modified ?>",
+					"description": "<?php echo get_the_excerpt( $post->ID ) ?>",
+					"articleBody": "<?php echo esc_html( $post->post_content ) ?>",
+					"author": {
+						"@type": "Person",
+						"name": "<?php echo the_author_meta( 'user_nicename' , $post->post_author ) ?>"
+					}
+				}
+				</script>
+			<?php } 
+		}
+
+	}
+	
+	// finally instantiate our plugin class and add it to the set of globals
+	$GLOBALS['WP_Post_LD'] = new WP_Post_LD();
 }
